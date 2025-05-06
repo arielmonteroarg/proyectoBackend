1a4266ff15867b34107fc6bf85953722b1a45bc0
arielmonteroarg <arielmontero.arg@gmail.com>
2025-05-06 14:03:49 -0300

    proyecto final 1.0.2

--- src/models/Usuarios.js
+++ src/models/Usuarios.js
@@ -5,7 +5,7 @@
 import mongoose from 'mongoose';
 
 const usuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
+  nombre: { type: String, required: true , index: true},
   edad: { type: Number, required: true },
   sexo: { type: String, enum: ['M', 'F'], required: true },
 });
